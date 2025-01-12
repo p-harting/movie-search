918a60ff62e70b5bfd70c96e5e5ca914b4d66311
fogfox <fogfox@pop-os.fritz.box>
2025-01-12 12:28:45 +0100

    Add favorites page and install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Home from "./pages/Home"
+import {Routes, Route} from "react-router-dom"
+import Favorites from './pages/Favorites'
 
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/favorites" element={<Favorites />}/>
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+    return(
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet</h2>
+            <p>Start adding movies by pressing the heart button</p>
+        </div>
+    )
+}
+
+export default Favorites
