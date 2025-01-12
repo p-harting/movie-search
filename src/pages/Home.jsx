66b40084f370430356be7835b17d302ef1ac7279
fogfox <fogfox@pop-os.fritz.box>
2025-01-12 12:16:28 +0100

    Add search form with useState

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,41 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
-function Home () {
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
-        {id: 1, title: "Movie 1", release: "2024"},
-        {id: 2, title: "Movie 2", release: "2025"},
-        {id: 3, title: "Movie 3", release: "1998"},
+        { id: 1, title: "Movie 1", release: "2024" },
+        { id: 2, title: "Movie 2", release: "2025" },
+        { id: 3, title: "Movie 3", release: "1998" },
     ]
-    return(
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+    }
+
+    return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit"
+                    className="search-buttons">Search</button>
+            </form>
+
             <div className="movies-grid">
-                {movies.map(movie => (
-                <MovieCard movie={movie} key={movie.id}/>
-                ))}
+                {movies.map(
+                    movie => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    )
+                )}
             </div>
         </div>
-    )
+    );
 }
 
 export default Home
